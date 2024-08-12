91f1558163b09ac3863d52964d0b345ac8e07380
Erildo07 <erildonunes12gmail.com>
2024-08-11 22:39:34 -0300

    multiplicação

--- main.c
+++ main.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
-int main(){
 
-	int num1, num2, num3, num4;
-	
-	printf("Digite quatro numeros, de espaco para cada numero digitado:");
-	scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
-	
-	if(num1 > num2 && num1 > num3 && num1 > num4){
-		
-		printf("%d foi o maior numero digitado\n",num1);
-	}
-	else if(num2 > num1 && num2 > num3 && num2 > num4){
-		
-		printf("%d foi o maior numero digitado\n",num2);
-	}
-	else if(num3 > num2 && num3 > num1 && num3 > num4){
-		
-		printf("%d foi o maior numero digitado\n",num3);
-	}
-	else if(num4 > num2 && num4 > num3 && num4 > num1){
-		
-		printf("%d foi o maior numero digitado\n",num4);
-	}
-	
-return 0;
+int main() {
+   float num1,num2, mult;
+   printf("Digite o número:\n");
+   scanf("%f", &num1);
+   
+   printf("Digite o número:\n");
+   scanf("%f", &num2);
+   
+   mult=num1*num2;
+   printf("Seu resultdo da mult e % 2f", mult);
+    return 0;
 }
